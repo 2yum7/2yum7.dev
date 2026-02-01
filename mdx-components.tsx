8b15df9cd3537fce07bfb501fcd507b5297a2b90
2yum7 <215ibuki@gmail.com>
2026-02-01 10:34:34 +0000

    Initial Commit

---
+++ mdx-components.tsx
@@ -0,0 +1,36 @@
+// import type { MDXComponents } from 'mdx/types'
+ 
+// const components: MDXComponents = {}
+ 
+// export function useMDXComponents(): MDXComponents {
+//   return components
+// }
+
+
+
+import type { MDXComponents } from 'mdx/types'
+import { Sacramento } from 'next/font/google'
+import Image, { ImageProps } from 'next/image'
+ 
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including inline styles,
+// components from other libraries, and more.
+ 
+const components = {
+  // Allows customizing built-in components, e.g. to add styling.
+  h1: ({ children }) => (
+    <h1 style={{ color: 'red', fontSize: '48px' }}>{children}</h1>
+  ),
+  img: (props) => (
+    <Image
+      sizes="100vw"
+      style={{ width: '100%', height: 'auto' }}
+      {...(props as ImageProps)}
+    />
+  ),
+} satisfies MDXComponents
+ 
+export function useMDXComponents(): MDXComponents {
+  return components
+}
