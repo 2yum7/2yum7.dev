bda34d7c8af8513bc60af7a6b62e033aadb57afe
2yum7 <215ibuki@gmail.com>
2026-02-12 22:00:09 +0000

    styles: toc styles change

--- components/toc.tsx
+++ components/toc.tsx
@@ -9,14 +9,18 @@ export default function Toc() {
       tocSelector: ".toc",
       contentSelector: ".post",
       headingSelector: "h2, h3",
+      scrollSmoothOffset: -100,
+      headingsOffset: 100,
     });
 
     return () => tocbot.destroy();
   }, []);
   return (
-    <div className='sticky top-0 pt-16 ml-8'>
-      <h2 className='text-xl border-l-4 border-secondary pl-1'>Toc</h2>
-      <div className='toc px-0 pb-8 text-base'></div>
+    <div className='sticky top-24 ml-12'>
+      <h2 className='mb-3 pl-3 py-1 text-sm font-semibold tracking-wide border-l-4 border-secondary'>
+        TOC
+      </h2>
+      <div className='toc px-1 py-2 text-sm leading-6'></div>
     </div>
   );
 }
